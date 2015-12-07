3becbd94ea8c649bfd33571ad82f82db593c092a
Guillermo García Franco <guigarfr@gmail.com>
2015-12-07 12:46:32 +0100

    google mail config in prod

--- scitweets/settings/production.py
+++ scitweets/settings/production.py
@@ -191,3 +191,12 @@ ALLOWED_HOSTS = ['*']
 ADMINS = (('Guille Scitweets','guigarfr+scitweets@gmail.com'),)
 
 MANAGERS = ADMINS
+
+
+# Google email
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'guigarfr@gmail.com'
+EMAIL_HOST_PASSWORD = 'mbjiszeasdnpxtho'
+
